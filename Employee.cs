26bc4241edbe3be5951fce24c0e224919671f0f7
Sahil Hirpara <sahilhirpara42@gmail.com>
2025-05-20 11:46:53 -0400

    Updated logic

---
+++ Employee.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab1
+{
+    
+    // I, Sahil Hirpara, 000123456 certify that this material is my original work.No other person's work has been used without due acknowledgement.
+   
+    /// <summary>
+    /// Employee modal class containg all method, constructor, attributes and many more
+    /// In this class information regarding employyes is stored, alltered and displayed
+    /// </summary>
+    internal class Employee
+    {
+        /// <summary>
+        /// Attributes: name: is for employee name, number: is for employee number, rate: is for employee rate, hours: is for employees hours, gross is for employees gross income
+        /// </summary>
+        private string name;
+        private int number;
+        private decimal rate;
+        private double hours;
+        private decimal gross;
+
+        /// <summary>
+        /// Constructor for allocating memory to variables
+        /// </summary>
+        /// <param name="name">employee name</param>
+        /// <param name="number">employee number</param>
+        /// <param name="rate">employee rate</param>
+        /// <param name="hours">employees hours</param>
+        public Employee(string name, int number, decimal rate, double hours) {
+            this.name = name;   
+            this.number = number;   
+            this.rate = rate;   
+            this.hours = hours; 
+        }   
+
+        /// <summary>
+        /// calculates Gross income of employee
+        /// </summary>
+        /// <returns>gross</returns>
+        public decimal GetGross()
+        {
+            if (hours > 40)
+            {
+                gross = rate * 40 + (rate * (decimal)1.5 * (decimal)(hours - 40));
+                return gross;   
+            }
+            else
+            {
+                gross = rate * (decimal)hours;
+                return gross;
+            }
+        }
+
+        /// <summary>
+        /// returns hours
+        /// </summary>
+        /// <returns>hours</returns>
+        public double GetHours()
+        {
+            return hours;
+        }
+
+        /// <summary>
+        /// returns employee name
+        /// </summary>
+        /// <returns>name</returns>
+        public string GetName()
+        {
+            return name;
+        }
+        /// <summary>
+        /// returns employee number
+        /// </summary>
+        /// <returns>number</returns>
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        /// <summary>
+        /// returns employee rate
+        /// </summary>
+        /// <returns>rate</returns>
+        public decimal GetRate(){ 
+            return rate;
+        }
+
+        /// <summary>
+        /// returns all data regarding employee in string format
+        /// </summary>
+        /// <returns>name,number,rate,hour,gross</returns>
+        public string toString()
+        {
+            //return name + number + rate + hours ;
+            return $"{name,-20} {number} {rate,6} {hours,6} {gross,6} ";
+        }
+
+        /// <summary>
+        /// sets employee hours
+        /// </summary>
+        /// <returns>hours</returns>
+        public double SetHours() { 
+        return hours;
+        }
+
+        /// <summary>
+        /// sets employee name
+        /// </summary>
+        /// <returns>name</returns>
+        public string SetName() { 
+            return name; 
+        }
+
+        /// <summary>
+        /// sets employee number
+        /// </summary>
+        /// <returns>number</returns>
+        public int SetNumber()
+        {
+            return number;
+        }
+
+        /// <summary>
+        /// sets employee rate
+        /// </summary>
+        /// <returns>rate</returns>
+        public decimal SetRate()
+        { 
+            return rate;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Program is the view class containg method that performs operations like reading and sorting data
+    /// </summary>
+    internal class Program
+
+    // I, Sahil Hirpara, 000123456 certify that this material is my original work.No other person's work has been used without due acknowledgement.
+
+    {
+        /// <summary>
+        /// DATAFILE stores the data from.csv file and array is created of name employee
+        /// </summary>
+        const string DATAFILE = "employees.txt";
+        static Employee[] employees = new Employee[100];
+
+        /// <summary>
+        /// It is a read method that reads data from DATAFILE and formats them and stores them in array employees for further use
+        /// </summary>
+        static void read()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(DATAFILE);
+                int index = 0;
+                while (!reader.EndOfStream && index < employees.Length)
+                {
+                    string[] items = reader.ReadLine().Split(',');
+
+
+                    employees[index++] = new Employee(items[0].Trim(), int.Parse(items[1].Trim()), decimal.Parse(items[2].Trim()), double.Parse(items[3].Trim()));
+
+                }
+
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// it is a sorting method performing insertion sort of data in employee and with help of switch case it give choices to user to select from which attribute they want their data to be fromatted and in the end it is displayed to user.
+        /// </summary>
+        /// <param name="employees"></param>
+        public static void sort(Employee[] employees)
+        {
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.Clear();
+                Console.Write("m e n u\n=======\n" +
+                                    "[1] - sort by employee name (ascending)\n" +
+                                    "[2] - sort by employee number (ascending)\n" +
+                                    "[3] - sort by employee pay rate (descending)\n" +
+                                    "[4] - sort by employee hours (descending)\n" +
+                                    "[5] - sort by employee gross pay (descending)\n" +
+                                    "[6] - exit program\n\n" +
+                                    "option : ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        for (int i = 1; i < employees.Length; i++)
+                        {
+                            if (employees[i] == null) continue;
+
+                            var key = employees[i];
+                            int j = i - 1;
+
+
+                            while (j >= 0 && employees[j] != null && string.Compare(employees[j].GetName(), key.GetName()) > 0)
+                            {
+                                employees[j + 1] = employees[j];
+                                j = j - 1;
+                            }
+                            employees[j + 1] = key;
+                        }
+
+                        Console.WriteLine("Sorted by Employee Name (Ascending):");
+                        foreach (var employee in employees)
+                        {
+                            if (employee != null)
+                                Console.WriteLine(employee.toString());
+                        }
+                        break;
+
+
+                    case "2":
+                        for (int i = 1; i < employees.Length; i++)
+                        {
+                            if (employees[i] == null) continue;
+
+                            var key = employees[i];
+                            int j = i - 1;
+
+
+                            while (j >= 0 && employees[j] != null && (employees[j].GetNumber() > key.GetNumber()))
+                            {
+                                employees[j + 1] = employees[j];
+                                j = j - 1;
+                            }
+                            employees[j + 1] = key;
+                        }
+
+                        Console.WriteLine("Sorted by Employee Number (Ascending):");
+                        foreach (var employee in employees)
+                        {
+                            if (employee != null)
+                                Console.WriteLine(employee.toString());
+                        }
+                        break;
+
+                    case "3":
+                        for (int i = 1; i < employees.Length; i++)
+                        {
+                            if (employees[i] == null) continue;
+
+                            var key = employees[i];
+                            int j = i - 1;
+
+
+                            while (j >= 0 && employees[j] != null && (employees[j].GetRate() < key.GetRate()))
+                            {
+                                employees[j + 1] = employees[j];
+                                j = j - 1;
+                            }
+                            employees[j + 1] = key;
+                        }
+
+                        Console.WriteLine("Sorted by Employee Pay Rate (Descending):");
+                        foreach (var employee in employees)
+                        {
+                            if (employee != null)
+                                Console.WriteLine(employee.toString());
+                        }
+
+                        break;
+
+                    case "4":
+                        for (int i = 1; i < employees.Length; i++)
+                        {
+                            if (employees[i] == null) continue;
+
+                            var key = employees[i];
+                            int j = i - 1;
+
+
+                            while (j >= 0 && employees[j] != null && (employees[j].GetHours() < key.GetHours()))
+                            {
+                                employees[j + 1] = employees[j];
+                                j = j - 1;
+                            }
+                            employees[j + 1] = key;
+                        }
+
+                        Console.WriteLine("Sorted by Employee Hours (Descending):");
+                        foreach (var employee in employees)
+                        {
+                            if (employee != null)
+                                Console.WriteLine(employee.toString());
+                        }
+                        break;
+
+                    case "5":
+                        for (int i = 1; i < employees.Length; i++)
+                        {
+                            if (employees[i] == null) continue;
+
+                            var key = employees[i];
+                            int j = i - 1;
+
+
+                            while (j >= 0 && employees[j] != null && (employees[j].GetGross() < key.GetGross()))
+                            {
+                                employees[j + 1] = employees[j];
+                                j = j - 1;
+                            }
+                            employees[j + 1] = key;
+                        }
+
+                        Console.WriteLine("Sorted by Employee Gross rate (Descending):");
+                        foreach (var employee in employees)
+                        {
+                            if (employee != null)
+                                Console.WriteLine(employee.toString());
+                        }
+                        break;
+
+                    case "6":
+                        flag = false;
+                        break;
+
+
+                    default:
+                        Console.Error.WriteLine("invalid option");
+                        Console.Beep(); ;
+                        break;
+                }
+                Console.Write("\n hit any key to continue ... ");
+                Console.ReadKey();
+            }
+            Console.Write("\n bye");
+
+        }
+
+        /// <summary>
+        /// It is the main method of the application which calls two method; read() and sort()
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            read();
+            sort(employees);
+
+
+        }
+    }
+}
